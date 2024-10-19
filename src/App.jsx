00da0e26acf96e236e12f914153e05a531b291be
Oleh Purys <wenger1901@gmail.com>
2024-10-19 12:03:05 +0300

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchContacts } from './redux/contactsOps';
-import { selectError, selectLoading } from './redux/contactsSlice';
 import Loader from './components/Loader/Loader';
+import { fetchContacts } from './redux/contacts/operations';
+import { selectError, selectLoading } from './redux/contacts/selectors';
 
 const App = () => {
   const dispatch = useDispatch();
--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,6 +1,5 @@
-import { createSelector, createSlice, isAnyOf } from '@reduxjs/toolkit';
-import { fetchContacts, addContact, deleteContact } from './contactsOps';
-import { selectNameFilter } from './filtersSlice';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { addContact, deleteContact, fetchContacts } from './operations';
 
 const initialState = {
   items: [],
@@ -60,16 +59,3 @@ const slice = createSlice({
 });
 
 export default slice.reducer;
-
-export const selectContacts = state => state.contacts.items;
-export const selectLoading = state => state.contacts.loading;
-export const selectError = state => state.contacts.error;
-
-export const selectFilteredContacts = createSelector(
-  [selectContacts, selectNameFilter],
-  (contacts, filter) => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-);
